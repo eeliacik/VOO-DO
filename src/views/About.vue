241030bb5f45b951622946fd8ef4a156be3f63eb
eeliacik <erhaneliacik@gmail.com>
2022-03-18 15:25:43 +0300

    flat list items completed

---
+++ src/views/About.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, quidem
+      deserunt! Necessitatibus nulla cupiditate dolores optio autem quo quasi
+      eum, repellat, at impedit corporis repudiandae dignissimos ducimus est,
+      fugiat harum?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, quidem
+      deserunt! Necessitatibus nulla cupiditate dolores optio autem quo quasi
+      eum, repellat, at impedit corporis repudiandae dignissimos ducimus est,
+      fugiat harum?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, quidem
+      deserunt! Necessitatibus nulla cupiditate dolores optio autem quo quasi
+      eum, repellat, at impedit corporis repudiandae dignissimos ducimus est,
+      fugiat harum?
+    </p>
+  </div>
+</template>
---
+++ src/views/Todo.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home pa-6">
+    <v-list flat>
+      <v-list-item v-for="task in tasks" :key="task.id">
+        <template v-slot:default="{ active }">
+          <v-list-item-action>
+            <v-checkbox :input-value="active" color="primary"></v-checkbox>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title> {{ task.title }}</v-list-item-title>
+          </v-list-item-content>
+        </template>
+      </v-list-item>
+    </v-list>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          title: 'Do stretches',
+        },
+        {
+          id: 2,
+          title: 'Eat breakfast',
+        },
+        {
+          id: 3,
+          title: 'Go to butcher',
+        },
+      ],
+    };
+  },
+};
+</script>
